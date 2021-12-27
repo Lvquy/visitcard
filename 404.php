4290cfd9098f42e1a32c3b99b52344e2b950f189
LvQuy <quyseo.ictu@gmail.com>
2021-12-27 15:10:44 +0700

    add 404 page

---
+++ 404.php
@@ -0,0 +1,36 @@
+<?php
+    include('includes/php.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php include('includes/head.html');?>
+<title>VN Card | 404</title>
+</head>
+<body>
+	  <!-- ***** Header Area Start ***** -->
+  <?php include('includes/menu.html');?>
+  <!-- ***** Header Area End ***** -->
+  <div class="main-banner ">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12"><h2 class="text-center fw-bold">Lỗi không tìm thấy trang!</h2></div>
+        	<div class="col-12 mt-2 text-center" >
+        		<h4>Liên kết bạn truy cập không có trong hệ thống, hoặc không còn nữa</h4>
+        		<h4>Vui lòng kiểm tra lại</h4>
+        		<a href="." class="btn btn-primary mt-3">Bấm vào đây để vào trang chủ</a> 
+        		<br>
+        		<br>
+        		<br>
+        		<hr>
+        		<p>https://vncard.info - Thẻ cá nhân thông minh 01 chạm</p>
+        	</div>
+        </div>
+      </div>
+    </div>   
+</body>
+    <!--   footer start -->
+    <?php include('includes/footer.html'); ?>
+    <?php include('includes/script.html');?>
+    <!--   footer end -->
+</html>
