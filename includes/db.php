4fcbc72e3e58ec102f2371068830249d8b5af967
LvQuy <quyseo.ictu@gmail.com>
2022-01-09 08:41:01 +0700

    fix bg top

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,12 @@
  <?php
  
 // local
-/*
-$con = mysqli_connect("localhost","root","","smartcard");
-if (mysqli_connect_errno()){
-  echo "Không thể kết nối đến MySQL: " . mysqli_connect_error();
-}
-*/
+
+// $con = mysqli_connect("localhost","root","","smartcard");
+// if (mysqli_connect_errno()){
+//   echo "Không thể kết nối đến MySQL: " . mysqli_connect_error();
+// }
+
 
 
 //server
